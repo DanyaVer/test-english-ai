74bde431084cf4f2e528b57a023795b71fd7e25b
DanyaVer <danyavernik@gmail.com>
2023-01-22 20:28:27 +0200

    d

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,31 @@
+//webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": [
-                            "@babel/preset-env",
-                            "@babel/preset-react"
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|jp(e*)g|svg|gif)$/,
-                use: ['file-loader'],
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: path.join(__dirname, "src", "index.html"),
-        }),
-    ],
-    devServer: {
-        port: 3030,
-        liveReload: true
-    },
-};
+   entry: './src/index.js',
+   output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js'
+   },
+   devServer: {
+      port: 8080
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+         },
+         {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+   },
+   plugins:[
+       new HtmlWebpackPlugin({
+            template: path.join(__dirname,'/src/index.html')
+       }) 
+   ]
+}
