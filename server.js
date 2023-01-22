6b8372caf7df4e8749a124d615b0c023352d1148
DanyaVer <danyavernik@gmail.com>
2023-01-22 19:43:37 +0200

    builable v

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var path = require('path');
-var express = require('express');
+const path = require('path');
+const express = require('express');
 
-var app = express();
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.set('port', process.env.PORT || 8080);
 
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },  
+  plugins:[
+    new HtmlWebPackPlugin({
+      template: path.resolve( __dirname, 'public/index.html' ),
+      filename: 'index.html'
+    })
+  ],
+  module:{
+      rules:[
+        {
+          test:/\.js$/,
+          exclude:/node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env','@babel/preset-react']
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
-    ]
-};
+          }
+        },
+      ]
+  },
+  devServer: {},
+}
