6ee041cc6a7b2b5ea3be3db05af01b17a07b0a7f
DanyaVer <danyavernik@gmail.com>
2023-01-22 20:57:27 +0200

    fd

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
-
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 3030);
-
-const server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+const publicPath = path.join(__dirname, '.', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join('', 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
 });
